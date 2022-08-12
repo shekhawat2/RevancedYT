5d23e6c9b33e039d35c8feea1aee21a5a5ad0255
Anand Shekhawat <anandsingh215@yahoo.com>
2022-08-13 00:48:57 +0530

    add revanced youtube-music

---
+++ RevancedYTM/customize.sh
@@ -0,0 +1,23 @@
+# Unmount Old ReVanced
+stock_path=$( pm path com.google.android.apps.youtube.music | grep base | sed 's/package://g' )
+if [[ '$stock_path' ]] ; then umount -l $stock_path; fi
+
+# Uninstall Old YoutubeMusic
+pm uninstall com.google.android.apps.youtube.music > /dev/null | true
+
+# Install YoutubeMusic
+TPDIR=$MODPATH/youtube-music
+SESSION=$(pm install-create -r | grep -oE '[0-9]+')
+APKS="$(ls $TPDIR)"
+for APK in $APKS; do
+pm install-write $SESSION $APK $TPDIR/$APK
+done
+pm install-commit $SESSION
+rm -rf $TPDIR
+
+# Mount for Now
+base_path=$MODPATH/revanced-music.apk
+stock_path=$( pm path com.google.android.apps.youtube.music | grep base | sed 's/package://g' )
+chcon u:object_r:apk_data_file:s0 $base_path
+mount -o bind $base_path $stock_path
+am force-stop com.google.android.apps.youtube.music
