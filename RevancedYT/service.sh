7320113905b01a80229616eed6d58f6394b08c72
Nguyen Hoang The Vi <84650617+HuskyDG@users.noreply.github.com>
2022-09-01 14:42:08 +0530

    [skip ci] service.sh: mount bind revanced.apk from magisk's mirror
    Some banking apps detect the mountpoint of youtube base apk to
    determinedevice is rooted. So mount bind from magisk's mirror to
    trigger magisk unmount and let it hidden along with magisk hiding.

--- RevancedYT/service.sh
+++ RevancedYT/service.sh
@@ -1,7 +1,9 @@
 #!/system/bin/sh
+MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
+MIRROR="$MAGISKTMP/.magisk/mirror"
 while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 1; done
 
 base_path="/data/adb/modules/revanced/revanced.apk"
 stock_path=$( pm path com.google.android.youtube | grep base | sed 's/package://g' )
 chcon u:object_r:apk_data_file:s0 $base_path
-mount -o bind $base_path $stock_path
+mount -o bind $MIRROR$base_path $stock_path
