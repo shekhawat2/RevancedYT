f707247cd46d75c75098766d8778823610440b4c
Anand Shekhawat <anandsingh215@yahoo.com>
2022-09-05 20:52:54 +0530

    [skip ci] split apk for youtube module

--- yt.sh
+++ yt.sh
@@ -32,9 +32,9 @@ get_latestytmversion() {
 
 dl_yt() {
     rm -rf $2
-    echo "Downloading YouTube $1"
+    echo "Downloading YouTube $1 $3"
     url="https://www.apkmirror.com/apk/google-inc/youtube/youtube-${1//./-}-release/"
-    url="$url$(req "$url" - | grep Variant -A50 | grep ">APK<" -A2 | grep android-apk-download | sed "s#.*-release/##g;s#/\#.*##g")"
+    url="$url$(req "$url" - | grep Variant -A50 | grep ">$3<" -A2 | grep android-apk-download | sed "s#.*-release/##g;s#/\#.*##g")"
     url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
     url="https://www.apkmirror.com$(req "$url" - | tr '\n' ' ' | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
     req "$url" "$2"
@@ -148,8 +148,13 @@ find $CURDIR -type f -name *.zip -exec rm -rf {} \;
 rm -rf $YTMODULEPATH/youtube && mkdir -p $YTMODULEPATH/youtube
 rm -rf $YTMMODULEPATH/youtube-music && mkdir -p $YTMMODULEPATH/youtube-music
 
-# Download Youtube
-dl_yt $YTVERSION $YTMODULEPATH/youtube/base.apk
+# Download Youtube bundle
+dl_yt $YTVERSION $CURDIR/$YTVERSION.zip BUNDLE
+unzip -j -q $CURDIR/$YTVERSION.zip *.apk -d $YTMODULEPATH/youtube || exit 1
+rm $CURDIR/$YTVERSION.zip
+
+# Download Youtube apk
+dl_yt $YTVERSION $CURDIR/$YTVERSION.apk APK
 
 # Download Youtube Music
 dl_ytm $YTMVERSION $CURDIR/$YTMVERSION.apk
@@ -208,7 +213,7 @@ java -jar $CLI \
 
 # NoRoot
 java -jar $CLI \
-    -a $YTMODULEPATH/youtube/base.apk \
+    -a $CURDIR/$YTVERSION.apk \
     -o $CURDIR/${YTNAME}-noroot.apk \
     --keystore=$CURDIR/revanced.keystore \
     -b $PATCHES \
