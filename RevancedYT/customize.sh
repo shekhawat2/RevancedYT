af8c6f32f9f9528e0f1152ad59a1886513f70903
Anand Shekhawat <anandsingh215@yahoo.com>
2022-10-18 09:17:01 +0530

    test

--- RevancedYT/customize.sh
+++ RevancedYT/customize.sh
@@ -1,27 +1,31 @@
-PACKAGE_NAME=com.google.android.youtube
+NAME=_NAME_
+PACKAGE_NAME=_PACKAGE_NAME_
 
 # Unmount Old ReVanced
 stock_path=$( pm path $PACKAGE_NAME | grep base | sed 's/package://g' )
 if [[ '$stock_path' ]] ; then umount -l $stock_path; fi
 
-# Install Youtube
-ui_print "Installing Stock Youtube..."
+# Uninstall Old $NAME
+pm uninstall -k $PACKAGE_NAME > /dev/null | true
+
+# Install $NAME
+ui_print "Installing Stock $NAME..."
 SESSION=$(pm install-create -r | grep -oE '[0-9]+')
-APKS="$(ls $MODPATH/youtube)"
+APKS="$(ls $MODPATH/$NAME)"
 for APK in $APKS; do
-pm install-write $SESSION $APK $MODPATH/youtube/$APK > /dev/null
+pm install-write $SESSION $APK $MODPATH/$NAME/$APK > /dev/null
 done
 pm install-commit $SESSION
 
 # Merge Patch
-ui_print "Patching Stock Youtube..."
+ui_print "Patching Stock $NAME..."
 BSPATCH=$MODPATH/tools/bspatch
 chmod +x $BSPATCH
-$BSPATCH $MODPATH/youtube/base.apk $MODPATH/revanced.apk $MODPATH/diff.patch
-rm -rf $MODPATH/youtube $MODPATH/tools $MODPATH/diff.patch
+$BSPATCH $MODPATH/$NAME/base.apk $MODPATH/$NAME.apk $MODPATH/diff.patch
+rm -rf $MODPATH/$NAME $MODPATH/tools $MODPATH/diff.patch
 
 # Mount for Now
-base_path=$MODPATH/revanced.apk
+base_path=$MODPATH/$NAME.apk
 stock_path=$( pm path $PACKAGE_NAME | grep base | sed 's/package://g' )
 chcon u:object_r:apk_data_file:s0 $base_path
 mount -o bind $base_path $stock_path
--- RevancedYT/service.sh
+++ RevancedYT/service.sh
@@ -1,11 +1,12 @@
 #!/system/bin/sh
-PACKAGE_NAME=com.google.android.youtube
+NAME=_NAME_
+PACKAGE_NAME=_PACKAGE_NAME_
 
 MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
 MIRROR="$MAGISKTMP/.magisk/mirror"
 while [ "$(getprop sys.boot_completed | tr -d '\r')" != "1" ]; do sleep 1; done
 
-base_path="/data/adb/modules/revanced/revanced.apk"
+base_path="/data/adb/modules/$NAME/$NAME.apk"
 stock_path=$( pm path $PACKAGE_NAME | grep base | sed 's/package://g' )
 chcon u:object_r:apk_data_file:s0 $base_path
 mount -o bind $MIRROR$base_path $stock_path
