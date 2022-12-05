bd96dc8f59219159da9058b66538fbfc2d4dc5d7
Takeda-senpai <deadboltsx303@gmail.com>
2022-12-05 22:33:54 +0700

    Dynamic mount implement
    - Only apply bind mount when YouTube process is started, other processes can't see the bind mount
    - Workaround soft reboot, YouTube ReVanced cannot be opened after soft reboot
    - Workaround app update/uninstall, bind mounted files cannot be deleted

--- RevancedYTM/customize.sh
+++ RevancedYTM/customize.sh
@@ -1,11 +1,18 @@
 PACKAGE_NAME=com.google.android.apps.youtube.music
 
-# Unmount Old ReVanced
-stock_path=$( pm path $PACKAGE_NAME | grep base | sed 's/package://g' )
-if [[ '$stock_path' ]] ; then umount -l $stock_path; fi
+if [ "$BOOTMODE" != "true" ]; then
+    abort "! Recovery install is not supported"
+fi
 
 # Uninstall Old YoutubeMusic
 pm uninstall -k $PACKAGE_NAME > /dev/null | true
+MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
+
+if [ ! -d "$MAGISKTMP/.magisk/modules/magisk_proc_monitor" ]; then
+    ui_print "! Please install Magisk Process monitor tool v1.1+"
+    ui_print "  https://github.com/HuskyDG/magisk_proc_monitor"
+    abort
+fi
 
 # Install YoutubeMusic
 ui_print "Installing Stock Youtube Music..."
@@ -22,10 +29,3 @@ BSPATCH=$MODPATH/tools/bspatch
 chmod +x $BSPATCH
 $BSPATCH $MODPATH/youtube-music/base.apk $MODPATH/revanced-music.apk $MODPATH/diff.patch
 rm -rf $MODPATH/youtube-music $MODPATH/tools $MODPATH/diff.patch
-
-# Mount for Now
-base_path=$MODPATH/revanced-music.apk
-stock_path=$( pm path $PACKAGE_NAME | grep base | sed 's/package://g' )
-chcon u:object_r:apk_data_file:s0 $base_path
-mount -o bind $base_path $stock_path
-am force-stop $PACKAGE_NAME
