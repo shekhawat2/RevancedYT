9f66bbeb575b6060fbf7353fead4b689154d526b
Harumi <84650617+HuskyDG@users.noreply.github.com>
2022-12-13 20:26:32 +0700

    Dynamic mount implement (#9)
    - Only apply bind mount when YouTube process is started, other processes can't see the bind mount
    - Workaround soft reboot, YouTube ReVanced cannot be opened after soft reboot
    - Workaround app update/uninstall, bind mounted files cannot be deleted

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 YouTube + YouTube Music
 
+Require [Magisk Process Monitor tool](https://github.com/HuskyDG/magisk_proc_monitor) v1.1+
+
 Official supported version (Release) script runs on Sunday at 05:30AM(IST).\
 Experimental latest version (Pre-release) script runs on Monday and Friday at 07:00AM(IST).\
 The script will fetch the latest version available.
--- RevancedYT/customize.sh
+++ RevancedYT/customize.sh
@@ -1,8 +1,16 @@
 PACKAGE_NAME=com.google.android.youtube
 
-# Unmount Old ReVanced
-stock_path=$( pm path $PACKAGE_NAME | grep base | sed 's/package://g' )
-if [[ '$stock_path' ]] ; then umount -l $stock_path; fi
+if [ "$BOOTMODE" != "true" ]; then
+    abort "! Recovery install is not supported"
+fi
+
+MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
+
+if [ ! -d "$MAGISKTMP/.magisk/modules/magisk_proc_monitor" ]; then
+    ui_print "! Please install Magisk Process monitor tool v1.1+"
+    ui_print "  https://github.com/HuskyDG/magisk_proc_monitor"
+    abort
+fi
 
 # Install Youtube
 ui_print "Installing Stock Youtube..."
@@ -19,10 +27,3 @@ BSPATCH=$MODPATH/tools/bspatch
 chmod +x $BSPATCH
 $BSPATCH $MODPATH/youtube/base.apk $MODPATH/revanced.apk $MODPATH/diff.patch
 rm -rf $MODPATH/youtube $MODPATH/tools $MODPATH/diff.patch
-
-# Mount for Now
-base_path=$MODPATH/revanced.apk
-stock_path=$( pm path $PACKAGE_NAME | grep base | sed 's/package://g' )
-chcon u:object_r:apk_data_file:s0 $base_path
-mount -o bind $base_path $stock_path
-am force-stop $PACKAGE_NAME
