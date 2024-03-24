1f100dc282b9b321b76d3cb7a2dfadc70d1e5a95
Anand Shekhawat <anandsingh215@yahoo.com>
2024-03-24 03:52:03 +0000

    yt.sh: fix apk downloading again

--- yt.sh
+++ yt.sh
@@ -37,7 +37,7 @@ dl_yt() {
     url="https://www.apkmirror.com/apk/google-inc/youtube/youtube-${1//./-}-release/"
     url="$url$(req "$url" - | grep Variant -A50 | grep ">APK<" -A2 | grep android-apk-download | sed "s#.*-release/##g;s#/\#.*##g")"
     url="https://www.apkmirror.com$(req "$url" - | grep "downloadButton" | grep "forcebaseapk" | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
-    url="https://www.apkmirror.com$(req "$url" - | grep "please click" | sed 's#.*href="\(.*key=[^"]*\)">.*#\1#')"
+    url="https://www.apkmirror.com$(req "$url" - | grep "please click" | sed 's#.*href="\(.*key=[^"]*\)">.*#\1#;s#amp;##p')"
     echo "URL: $url"
     req "$url" "$2"
     if [ ! -f $2 ]; then echo failed && exit 1; fi
@@ -49,7 +49,7 @@ dl_ytm() {
     url="https://www.apkmirror.com/apk/google-inc/youtube/youtube-music-${1//./-}-release/"
     url="$url$(req "$url" - | grep arm64 -A30 | grep youtube-music | head -1 | sed "s#.*-release/##g;s#/\".*##g")"
     url="https://www.apkmirror.com$(req "$url" - | grep "downloadButton" | grep "forcebaseapk" | sed -n 's;.*href="\(.*key=[^"]*\)">.*;\1;p')"
-    url="https://www.apkmirror.com$(req "$url" - | grep "please click" | sed 's#.*href="\(.*key=[^"]*\)">.*#\1#')"
+    url="https://www.apkmirror.com$(req "$url" - | grep "please click" | sed 's#.*href="\(.*key=[^"]*\)">.*#\1#;s#amp;##p')"
     echo "URL: $url"
     req "$url" "$2"
     if [ ! -f $2 ]; then echo failed && exit 1; fi
